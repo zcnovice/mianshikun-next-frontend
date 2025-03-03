e6f928af46963a27eea56289273420579e27f145
zcnovice <2739650633@qq.com>
2025-03-03 09:31:20 +0800

    页面-1.0

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -1,97 +1,169 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import {Button} from "antd";
+import {
+  CaretDownFilled,
+  DoubleRightOutlined,
+  GithubFilled,
+  InfoCircleFilled,
+  LogoutOutlined,
+  PlusCircleFilled,
+  QuestionCircleFilled,
+  SearchOutlined,
+} from "@ant-design/icons";
+import type { ProSettings } from "@ant-design/pro-components";
+import {
+  PageContainer,
+  ProCard,
+  ProConfigProvider,
+  ProLayout,
+  SettingDrawer,
+} from "@ant-design/pro-components";
+import {
+  Button,
+  ConfigProvider,
+  Divider,
+  Dropdown,
+  Input,
+  Popover,
+  theme,
+} from "antd";
+import React, { useState } from "react";
 
-export default function Home() {
+const SearchInput = () => {
+  const { token } = theme.useToken();
   return (
-    <main className={styles.main}>
-      <Button type="primary">Primary Button</Button>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div
+      key="SearchOutlined"
+      aria-hidden
+      style={{
+        display: "flex",
+        alignItems: "center",
+        marginInlineEnd: 24,
+      }}
+      onMouseDown={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+    >
+      <Input
+        style={{
+          borderRadius: 4,
+          marginInlineEnd: 12,
+        }}
+        prefix={<SearchOutlined />}
+        placeholder="搜索方案"
+        variant="borderless"
+      />
+      <PlusCircleFilled
+        style={{
+          fontSize: 24,
+        }}
+      />
+    </div>
+  );
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function BasicLayout({ children }: Props) {
+  const [settings, setSetting] = useState<Partial<ProSettings> | undefined>({
+    fixSiderbar: true,
+    layout: "mix",
+    splitMenus: true,
+  });
+
+  const [pathname, setPathname] = useState("/list/sub-page/sub-sub-page1");
+  const [num, setNum] = useState(40);
+  if (typeof document === "undefined") {
+    return <div />;
+  }
+  return (
+    <div
+      id="basicLayout"
+      style={{
+        height: "100vh",
+        overflow: "auto",
+      }}
+    >
+      <ProLayout
+        location={{
+          pathname,
+        }}
+        avatarProps={{
+          src: "https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg",
+          size: "small",
+          title: "七妮妮",
+          render: (props, dom) => {
+            return (
+              <Dropdown
+                menu={{
+                  items: [
+                    {
+                      key: "logout",
+                      icon: <LogoutOutlined />,
+                      label: "退出登录",
+                    },
+                  ],
+                }}
+              >
+                {dom}
+              </Dropdown>
+            );
+          },
+        }}
+        actionsRender={(props) => {
+          if (props.isMobile) return [];
+          if (typeof window === "undefined") return [];
+          return [
+            props.layout !== "side" && document.body.clientWidth > 1400 ? (
+              <SearchInput />
+            ) : undefined,
+            <InfoCircleFilled key="InfoCircleFilled" />,
+            <QuestionCircleFilled key="QuestionCircleFilled" />,
+            <GithubFilled key="GithubFilled" />,
+          ];
+        }}
+        headerTitleRender={(logo, title, _) => {
+          const defaultDom = (
+            <a>
+              {logo}
+              {title}
+            </a>
+          );
+          if (typeof window === "undefined") return defaultDom;
+          if (document.body.clientWidth < 1400) {
+            return defaultDom;
+          }
+          if (_.isMobile) return defaultDom;
+          return <>{defaultDom}</>;
+        }}
+        menuFooterRender={(props) => {
+          if (props?.collapsed) return undefined;
+          return (
+            <div
+              style={{
+                textAlign: "center",
+                paddingBlockStart: 12,
+              }}
+            >
+              <div>© 2021 Made with love</div>
+              <div>by Ant Design</div>
+            </div>
+          );
+        }}
+        onMenuHeaderClick={(e) => console.log(e)}
+        menuItemRender={(item, dom) => (
+          <div
+            onClick={() => {
+              setPathname(item.path || "/welcome");
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            {dom}
+          </div>
+        )}
+      >
+        {children}
+      </ProLayout>
+    </div>
   );
 }
