44d08111dc0a008b6163b8243f59c12119a84c46
zcnovice <2739650633@qq.com>
2025-03-15 11:21:07 +0800

    顶部导航栏

---
+++ config/menu.tsx
@@ -0,0 +1,37 @@
+import { MenuDataItem } from "@ant-design/pro-layout";
+
+
+// 菜单列表
+const menus = [
+  {
+    path: "/",
+    name: "主页",
+  },
+  {
+    path: "/banks",
+    name: "题库",
+  },
+  {
+    path: "/questions",
+    name: "题目",
+  },
+  {
+    name: "面试鸭",
+    path: "https://mianshiya.com",
+    target: "_blank",
+  },
+  {
+    path: "/admin",
+    name: "管理",
+    icon: <CrownOutlined />,
+    children: [
+      {
+        path: "/admin/user",
+        name: "用户管理",
+      }
+    ],
+  },
+] as MenuDataItem[];
+
+// 导出
+export default menus;
